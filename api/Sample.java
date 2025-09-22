ba9c6623c4d4d18d11224d486f5fe2a0acccb03c
github-trail <145013951+github-trail@users.noreply.github.com>
2025-09-22 14:41:07 +0530

    Update Sample.java

--- api/Sample.java
+++ api/Sample.java
@@ -1,5 +1,6 @@
 class Sample{
     public static void main(String[] args) {
         System.out.println("Hello, Github actions!");
+        System.out.println("Implemented the butler in github actions");
     }
 }
